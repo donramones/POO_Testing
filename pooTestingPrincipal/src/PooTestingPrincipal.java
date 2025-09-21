38e3ddeb4df6ac233ca3bfa09fd6c629a14b7031
donramones <agusrivarolaa00@gmail.com>
2025-09-21 12:42:57 -0300

    10ok

---
+++ pooTestingPrincipal/src/PooTestingPrincipal.java
@@ -0,0 +1,28 @@
+
+z/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pootestingprincipal;
+
+
+
+/**
+ *
+ * @author rivar
+ */
+public class PooTestingPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        cuentaBancaria cb = new cuentaBancaria();
+        cb.
+        
+        
+    }
+        
+        
+    
+}
